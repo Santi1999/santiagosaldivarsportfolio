503916a88f8f938735da7ec903e75b3b1202b5e6
Santi1999 <84410560+Santi1999@users.noreply.github.com>
2025-03-22 07:56:54 -0400

    Update astro.config.mjs
    changing site to santi1999.github.io and base adding / to repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,6 @@ export default defineConfig({
   },
   output: "server",
   adapter: vercel(),
-  site: "santiagosaldivar.com",
-  base: "santiagosaldivarsportfolio"
+  site: "santi1999.github.io",
+  base: "/santiagosaldivarsportfolio"
 })
