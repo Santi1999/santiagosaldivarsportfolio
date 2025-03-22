9c40bc4859829951bdaeb0f4ed6f368e41a70de4
Santi1999 <84410560+Santi1999@users.noreply.github.com>
2025-03-22 07:58:49 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,6 @@ export default defineConfig({
   },
   output: "server",
   adapter: vercel(),
-  site: "santi1999.github.io",
+  site: "santiagosaldivar.com",
   base: "/santiagosaldivarsportfolio"
 })
