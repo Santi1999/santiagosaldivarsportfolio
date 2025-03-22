b1a99bb4cf534ae0c90884996bb44f70ce9ee4ac
santi1999 <ssaldivar1@fordham.edu>
2025-03-22 08:53:27 -0400

    minor bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,15 @@ import { defineConfig, envField } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 import vercel from "@astrojs/vercel"
 
-const SERVER_PORT = 3000;
-const LOCALHOST = `http://localhost:${SERVER_PORT}`;
-const LIVE_URL = "https://santiagosaldivar.com";
-
-
-const SCRIPT = process.env.npm_lifecycle_script || "";
-const isBuild = SCRIPT.includes("astro build");
-let BASE_URL = LOCALHOST;
+const SERVER_PORT = 3000
+const LOCALHOST = `http://localhost:${SERVER_PORT}`
+const LIVE_URL = "https://santiagosaldivar.com"
+const SCRIPT = process.env.npm_lifecycle_script || ""
+const isBuild = SCRIPT.includes("astro build")
+let BASE_URL = LOCALHOST
 
 if (isBuild) {
-  BASE_URL = LIVE_URL;
+  BASE_URL = LIVE_URL
 }
 
 export default defineConfig({
@@ -28,5 +26,5 @@ export default defineConfig({
   output: "server",
   adapter: vercel(),
   site: BASE_URL,
-  base: "/"
-});
+  base: "",
+})
